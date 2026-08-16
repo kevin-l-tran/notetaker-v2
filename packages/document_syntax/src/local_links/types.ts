b30fa9c6fb4e28c3e782445ee04089d11ff8dc18
kevin-l-tran <kltran@csu.fullerton.edu>
2026-08-16 00:41:08 -0700

    biome auto fix

--- packages/document_syntax/src/local_links/types.ts
+++ packages/document_syntax/src/local_links/types.ts
@@ -1,17 +1,17 @@
 import type { Literal } from "mdast";
 
 export type LocalLink = Literal & {
-    type: "localLink";
-    mode: "manual" | "automatic" | "suppressed";
-    value: string;
-    target: string;
+	type: "localLink";
+	mode: "manual" | "automatic" | "suppressed";
+	value: string;
+	target: string;
 };
 
 declare module "mdast" {
-    interface RootContentMap {
-        localLink: LocalLink;
-    }
-    interface PhrasingContentMap {
-        localLink: LocalLink;
-    }
+	interface RootContentMap {
+		localLink: LocalLink;
+	}
+	interface PhrasingContentMap {
+		localLink: LocalLink;
+	}
 }
