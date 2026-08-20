f8222c5439683b7d8f7a06b54163eef1879b3622
kevin-l-tran <kltran@csu.fullerton.edu>
2026-08-19 21:55:30 -0700

    improved resolveMatches algo

--- packages/linking/src/matchers/resolve-matches.ts
+++ packages/linking/src/matchers/resolve-matches.ts
@@ -5,6 +5,8 @@ export default function resolveMatches(matches: AhoCorasickMatch[], reservedTarg
 
 	matches = matches.filter((m) => !reservedTargetIds.includes(m.pattern.targetId));
 
+	matches.sort((a, b) => a.start - b.start);
+
 	const subsumed: AhoCorasickMatch[] = [];
 	for (const match of matches) {
 		subsumed.push(
@@ -13,7 +15,18 @@ export default function resolveMatches(matches: AhoCorasickMatch[], reservedTarg
 	}
 	matches = matches.filter((m) => !subsumed.includes(m));
 
-	matches.sort((a, b) => a.start - b.start);
+	const overlapping: AhoCorasickMatch[] = [];
+	let previous = matches[0];
+	for (const match of matches) {
+		if (!previous) break;
+
+		if (match.start < previous.end) {
+			overlapping.push(match);
+		} else {
+			previous = match;
+		}
+	}
+	matches = matches.filter((m) => !overlapping.includes(m));
 
 	const linkedIds = new Set<string>();
 	for (const match of matches) {
