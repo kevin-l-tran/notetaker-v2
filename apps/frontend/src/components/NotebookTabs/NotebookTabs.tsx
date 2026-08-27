efed4c54c59ce22404cf79eea16009acacb13a28
kevin-l-tran <kltran@csu.fullerton.edu>
2026-08-27 01:25:28 -0700

    fixed formatting

--- apps/frontend/src/components/NotebookTabs/NotebookTabs.tsx
+++ apps/frontend/src/components/NotebookTabs/NotebookTabs.tsx
@@ -1,4 +1,4 @@
-import styles from "./NotebookTabs.module.css"
+import styles from "./NotebookTabs.module.css";
 
 export default function NotebookTabs() {
 	return <nav className={styles.tabs}>Workspace | Editor</nav>;
