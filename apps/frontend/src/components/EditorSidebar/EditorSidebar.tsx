a57819f2b8e9a89f7aaa17b0878293799701e7de
kevin-l-tran <kltran@csu.fullerton.edu>
2026-08-29 15:20:09 -0700

    added placeholder class descriptors

--- apps/frontend/src/components/EditorSidebar/EditorSidebar.tsx
+++ apps/frontend/src/components/EditorSidebar/EditorSidebar.tsx
@@ -1,23 +1,28 @@
 import { Button, Input } from "@base-ui/react";
 import styles from "./EditorSidebar.module.css";
 
+/*
+ *  Note: add semantic HTML later
+ *  For now, CSS classes will serve as element descriptors
+ */
+
 export default function EditorSidebar() {
 	return (
 		<aside className={styles.sidebar}>
 			<div className={styles.header}>
-				<div>Definitions</div>
-				<div>20</div>
+				<div className="title">Definitions</div>
+				<div className="nodeCount">20</div>
 			</div>
 
 			<Input placeholder="Search concepts..." />
 
-			<div>
-				<div>Item 1</div>
-				<div>Item 2</div>
-				<div>Item 3</div>
-				<div>Item 4</div>
-				<div>Item 5</div>
-      </div>
+			<div className="nodeList">
+				<div className="node">Item 1</div>
+				<div className="node">Item 2</div>
+				<div className="node">Item 3</div>
+				<div className="node">Item 4</div>
+				<div className="node">Item 5</div>
+			</div>
 
 			<Button>+ New Definition</Button>
 		</aside>
