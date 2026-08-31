10139f876b5a775fe35b19c9af128d34af7f04ef
kevin-l-tran <kltran@csu.fullerton.edu>
2026-08-30 23:15:41 -0700

    used done in health routes plugin

--- apps/backend/src/modules/health/health.routes.ts
+++ apps/backend/src/modules/health/health.routes.ts
@@ -1,11 +1,13 @@
 import type { FastifyPluginCallback } from "fastify";
 
-const healthRoutes: FastifyPluginCallback = (app) => {
+const healthRoutes: FastifyPluginCallback = (app, _options, done) => {
 	app.get("/health", () => {
 		return {
 			status: "ok",
 		};
 	});
+
+	done();
 };
 
 export default healthRoutes;
