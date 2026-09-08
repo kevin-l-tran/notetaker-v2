adc26a8be49ba0277d11806917a8d21ee7b35acf
kevin-l-tran <kltran@csu.fullerton.edu>
2026-09-07 20:43:15 -0700

    updated better auth to 1.7.3

---
+++ apps/backend/src/modules/auth/auth.cli.ts
@@ -0,0 +1,19 @@
+import { drizzleAdapter } from "@better-auth/drizzle-adapter/relations-v2";
+import { betterAuth } from "better-auth";
+import { env } from "../../config/env.ts";
+import { db } from "../../database/client.ts";
+import * as authSchema from "../../database/schema/auth.ts";
+
+export const auth = betterAuth({
+	database: drizzleAdapter(db, {
+		provider: "pg",
+		schema: authSchema,
+	}),
+	emailAndPassword: {
+		enabled: true,
+	},
+
+	baseURL: env.BETTER_AUTH_URL,
+	secret: env.BETTER_AUTH_SECRET,
+	trustedOrigins: [env.FRONTEND_URL],
+});
