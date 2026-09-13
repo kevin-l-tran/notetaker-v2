9d8a1c4af27f3463f80b8bf69d4baed2e6224fa5
kevin-l-tran <kltran@csu.fullerton.edu>
2026-09-13 03:09:40 -0700

    me route returns zod schema

--- apps/backend/src/modules/users/user.routes.ts
+++ apps/backend/src/modules/users/user.routes.ts
@@ -1,9 +1,11 @@
 import type { FastifyPluginCallback } from "fastify";
 import { getAuthenticatedUser } from "../auth/authenticatedUser.ts";
+import { toUserDTO } from "./user.mapper.ts";
 
 const userRoutes: FastifyPluginCallback = (app, _options, done) => {
 	app.get("/me", { preHandler: app.requireAuthentication }, (request, _reply) => {
-		return getAuthenticatedUser(request);
+		const appUser = getAuthenticatedUser(request);
+		return toUserDTO({ appUser });
 	});
 
 	done();
